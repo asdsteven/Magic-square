89f334eaaf0d0690c25cc684b3a5e9b5a4589815
asdsteven <asdsteven2@gmail.com>
2014-03-07 13:42:05 +0800

    Update gen_recipe.c

--- gen_recipe.c
+++ gen_recipe.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-void gen_recipe(int d)
+int d;
+int line[14][2];
+int a[36];
+int answer[15], list[14];
+
+int ids(int n, int used, int depth)
+{
+	if (n == d * 2 + 2)
+		return 1;
+	int i, j, s, x;
+	for (i = 0; i < d * 2 + 2; ++i)
+		if (~used & 1 << i) {
+			s = 0;
+			x = line[i][0];
+			for (j = 0; j < d; ++j) {
+				s += !a[x]++;
+				x += line[i][1];
+			}
+			if (s <= answer[n]) {
+				answer[n] = s;
+				if (n < depth && ids(n + 1, used | 1 << i, depth)) {
+					list[n] = i;
+					return 1;
+				}
+			}
+			x = line[i][0];
+			for (j = 0; j < d; ++j) {
+				--a[x];
+				x += line[i][1];
+			}
+		}
+	return 0;
+}
+
+void gen_recipe()
 {
 	int i, j, k;
-	int line[d * 2 + 2][2];
-	int stack[14 * 14], top;
-	int current[d * 2 + 2], count[d * 2 + 2], cur = 0;
-	int answer[d * 2 + 2], list[d * 2 + 2];
-	int a[d * d], used[d * 2 + 2];
-	int equal = 0, better = 0;
 	
-	/**
-	 * the i(th) line is described by
-	 * line[i][0] + n * line[i][1]
-	 */
 	line[0][0] = 0;
 	line[0][1] = d + 1;
 	for (i = 0; i < d; ++i) {
@@ -27,65 +51,21 @@ void gen_recipe(int d)
 	line[d * 2 + 1][1] = d - 1;
 	
 	memset(a, 0, sizeof(a));
-	memset(used, 0, sizeof(used));
-	for (i = 0; i < d * 2 + 2; ++i)
+	for (i = 0; i <= d * 2 + 2; ++i) {
 		answer[i] = 10000;
-	
-	for (top = 0; top < (d + 1) / 2 + 1; ++top)
-		stack[top] = top;
-	while (top) {
-		k = stack[top - 1];
-		if (k >= 10000) { /*visited*/
-			used[k -= 10000] = 0;
-			--top;
-			if (equal == cur)
-				--equal;
-			--cur;
-			j = line[k][0];
-			for (i = 0; i < d; ++i) {
-				--a[j];
-				j += line[k][1];
-			}
-			continue;
-		}
-		
-		j = line[k][0];
-		count[cur] = 0;
-		for (i = 0; i < d; ++i) {
-			count[cur] += !a[j]++;
-			j += line[k][1];
-		}
-		current[cur++] = k;
-		used[k] = 1;
-		stack[top - 1] += 10000; /*mark as visited*/
-		if (equal == cur - 1)
-			if (count[cur - 1] == answer[cur - 1])
-				++equal;
-			else if (count[cur - 1] > answer[cur - 1])
-				continue;
-		
-		for (i = 0; i < d * 2 + 2; ++i)
-			if (!used[i])
-				stack[top++] = i;
-		if (cur == d * 2 + 2 && equal < cur)
-			for (i = 0; i < cur; ++i) {
-				answer[i] = count[i];
-				list[i] = current[i];
-			}
+		ids(0, 0, i);
 	}
 	
+	memset(a, 0, sizeof(a));
 	for (i = 0; i < d * 2 + 2; ++i) {
 		if (answer[i] == 0) {
 			printf("-5, %d, %d,\n", line[list[i]][0], line[list[i]][1]);
 			continue;
 		}
-		j, k = line[list[i]][0];
+		k = line[list[i]][0];
 		for (j = 0; j < d; ++j) {
-			if (!a[k]) {
-				if (--answer[i])
-					printf("%d, ", k);
-				a[k] = 1;
-			}
+			if (!a[k]++ && --answer[i])
+				printf("%d, ", k);
 			k += line[list[i]][1];
 		}
 		printf("-3, %d, %d,\n", line[list[i]][0], line[list[i]][1]);
@@ -94,10 +74,9 @@ void gen_recipe(int d)
 
 int main()
 {
-	int i;
-	for (i = 3; i <= 6; ++i) {
-		printf("int recipe_book%d[] = {\n", i);
-		gen_recipe(i);
+	for (d = 3; d <= 6; ++d) {
+		printf("int recipe_book%d[] = {\n", d);
+		gen_recipe();
 		printf("};\n\n");
 		fflush(stdout);
 	}
