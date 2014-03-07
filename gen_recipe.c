493d282934653c2921f3914b4ed96e6e8f9536dd
asdsteven <asdsteven2@gmail.com>
2014-03-07 16:39:06 +0800

    Update gen_recipe.c

--- gen_recipe.c
+++ gen_recipe.c
@@ -1,3 +1,14 @@
+/**
+ * Used to generate "recipe.h".
+ * Recipes elements are of variable lengths :
+ * A		- exhaust element A
+ * -2 A		- next element should be greater than element A
+ *            to avoid symmetric isotopes. To be input manually.
+ * -3 A B	- calculate element in A + n * B
+ * -5 A B	- check if A + n * B is consistent
+ * -1		- end of recipe
+ */
+
 #include <stdio.h>
 #include <string.h>
 
@@ -55,7 +66,11 @@ void gen_recipe()
 		answer[i] = 10000;
 		ids(0, 0, i);
 	}
-	
+}
+
+void print_recipe()
+{
+	int i, j, k;
 	memset(a, 0, sizeof(a));
 	for (i = 0; i < d * 2 + 2; ++i)
 		if (answer[i] == 0) {
@@ -71,11 +86,26 @@ void gen_recipe()
 		}
 }
 
+void print_grid()
+{
+	int i, j;
+	printf("/**\n");
+	for (i = 0; i < d; ++i) {
+		printf(" *");
+		for (j = 0; j < d; ++j)
+			printf(" %2d", i * d + j);
+		printf("\n");
+	}
+	printf(" */\n");		
+}
+
 int main()
 {
 	for (d = 3; d <= 6; ++d) {
+		print_grid();
 		printf("int recipe_book%d[] = {\n", d);
 		gen_recipe();
+		print_recipe();
 		printf("};\n\n");
 		fflush(stdout);
 	}
