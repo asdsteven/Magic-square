8f93ae26f20250e10870214bdaad71db6222e5f1
asdsteven <asdsteven2@gmail.com>
2014-03-11 10:50:12 +0800

    Update gen_recipe.c

--- gen_recipe.c
+++ gen_recipe.c
@@ -2,11 +2,13 @@
  * Used to generate "recipe.h".
  * Recipes elements are of variable lengths :
  * A		- exhaust element A
- * -2 A		- next element should be greater than element A
- *            to avoid symmetric isotopes. To be input manually.
+ * -2 A	B	- states that element[A] < next_element <= B
+ *            to avoid symmetric isotopes
  * -3 A B	- calculate element in A + n * B
  * -5 A B	- check if A + n * B is consistent
  * -1		- end of recipe
+ *
+ * Manual modification to recipe.h is still needed
  */
 
 #include <stdio.h>
@@ -52,19 +54,33 @@ void gen_recipe()
 	
 	line[0][0] = 0;
 	line[0][1] = d + 1;
+	line[1][0] = d - 1;
+	line[1][1] = d - 1;
 	for (i = 0; i < d; ++i) {
-		line[i + 1][0] = i * d;
-		line[i + 1][1] = 1;
-		line[i + d + 1][0] = i;
-		line[i + d + 1][1] = d;
+		line[i + 2][0] = i * d;
+		line[i + 2][1] = 1;
+		line[i + d + 2][0] = i;
+		line[i + d + 2][1] = d;
 	}
-	line[d * 2 + 1][0] = d - 1;
-	line[d * 2 + 1][1] = d - 1;
 	
 	memset(a, 0, sizeof(a));
 	for (i = 0; i <= d * 2 + 2; ++i) {
 		answer[i] = 10000;
 		ids(0, 0, i);
+	}
+}
+
+void print_symmetry(int k)
+{
+	if (k == 0) {
+		printf("-2, 0, %d, ", d * d - d / 2 * 4 + 1);
+	} else if (k % (d + 1) == 0) {
+		if (k / (d + 1) < d / 2)
+			printf("-2, %d, %d, ", k - (d + 1), d * d - (d / 2 - k / (d + 1)) * 2 + 1);
+		else if (k / (d + 1) >= d - d / 2)
+			printf("-2, %d, %d, ", (d - 1 - k / (d + 1)) * (d + 1), d * d);
+	} else if (k % (d - 1) == 0) {
+		printf("-2, 0, %d, ", d * d);
 	}
 }
 
@@ -78,11 +94,15 @@ void print_recipe()
 		} else {
 			k = line[list[i]][0];
 			for (j = 0; j < d; ++j) {
-				if (!a[k]++ && --answer[i])
-					printf("%d, ", k);
+				if (!a[k]++) {
+					print_symmetry(k);
+					if (--answer[i])
+						printf("%d, ", k);
+					else
+						printf("-3, %d, %d,\n", line[list[i]][0], line[list[i]][1]);
+				}
 				k += line[list[i]][1];
 			}
-			printf("-3, %d, %d,\n", line[list[i]][0], line[list[i]][1]);
 		}
 }
 
